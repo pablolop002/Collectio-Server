859c4a689f80831089995387ab81a6d8d5c518da
pablolop002 <pablolop002@me.com>
2020-12-05 18:00:26 +0100

    Api login, check authorization, token and list categories and subcategories

--- routes/api.js
+++ routes/api.js
@@ -2,53 +2,112 @@
 
 // Load modules
 const express = require('express');
-const multer = require("multer");
+const multer = require('multer');
 //const mysql = require('mysql');
-const passport = require("passport");
 
 // Config
+const conf = require('../config/conf');
 const database = require('../config/databases');
-//const passport = require('../config/auth');
+const passport = require('../config/auth');
 const upload = multer();
 
 // Function includes
-const DAOUsers = require("../repositories/DAOUsers");
-const DAOGroups = require("../repositories/DAOGroups");
-const DAOCollections = require("../repositories/DAOCollections");
-const DAOItems = require("../repositories/DAOItems");
+const DAOUsers = require('../repositories/DAOUsers');
+const DAOCategories = require('../repositories/DAOCategories');
+const DAOCollections = require('../repositories/DAOCollections');
+const DAOItems = require('../repositories/DAOItems');
+const i18n = require("i18n");
 
 // Function instances
 const daoUsers = new DAOUsers(database.pool);
-const daoGroups = new DAOGroups(database.pool);
-//const daoCollections = new DAOCollections(database.pool);
+const daoCategories = new DAOCategories(database.pool);
+const daoCollections = new DAOCollections(database.pool);
 const daoItems = new DAOItems(database.pool);
 
 // Router
 const api = express.Router();
 
-// Check Authorization
-api.use(function (req, res, next) {
-    let token = req.header('Authorization');
+// Google API Auth
+api.get('/login/google', passport.authenticate('GoogleMobile', {
+    scope: ['https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email']
+}));
 
-    if (!token || token !== "") {
-        next(new Error("No auth key"));
+api.get('/login/google/callback', passport.authenticate('GoogleMobile'), function (request, response) {
+    daoUsers.createApikey(request.user.Id, function (err, token) {
+        if (err) {
+            response.redirect('collectioapp://#?err=' + err);
+        } else {
+            response.redirect('collectioapp://#?token=' + token.token);
+        }
+    });
+});
+
+// Apple API Auth
+api.get('/login/apple', passport.authenticate('AppleMobile'));
+
+api.post('/login/apple/callback', function (request, response, next) {
+    passport.authenticate('AppleMobile', function (err, user, info) {
+        if (err) {
+            response.redirect('collectioapp://#?err=' + err);
+        } else {
+            daoUsers.createApikey(user.Id, function (err, token) {
+                if (err) {
+                    response.redirect('collectioapp://#?err=' + err);
+                } else {
+                    response.redirect('collectioapp://#?token=' + token.token);
+                }
+            });
+        }
+    })(request, response, next);
+});
+
+// Check Authorization
+api.use(function (request, response, next) {
+    let token = request.header('Authorization');
+
+    if (!token || token !== conf.authApi) {
+        next(new Error(i18n.__('noApiAuth')));
     }
 
     next();
 });
 
-// Google API Auth
-api.get('/google', passport.authenticate('Google', {scope: ['profile']}));
+api.get('/categories', function (request, response, next) {
+    daoCategories.listCategories(function (err, categories) {
+        if (err) {
+            response.json({
+                'status': 'ko',
+                'code': 1,
+                'message': JSON.stringify(err)
+            });
+        } else {
+            response.json({
+                'status': 'ok',
+                'code': 0,
+                'data': JSON.stringify(categories)
+            })
+        }
+    })
+});
 
-api.get('/auth/google/callback',
-    passport.authenticate('google', {failureRedirect: '/login'}),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    });
-
-// Apple API Auth
-api.get("/apple", passport.authenticate('Apple'));
+api.get('/subcategories', function (request, response, next) {
+    daoCategories.listSubcategories(function (err, subcategories) {
+        if (err) {
+            response.json({
+                'status': 'ko',
+                'code': 1,
+                'message': JSON.stringify(err)
+            });
+        } else {
+            response.json({
+                'status': 'ok',
+                'code': 0,
+                'data': JSON.stringify(subcategories)
+            })
+        }
+    })
+});
 
 // Check Session
 api.use(function (req, res, next) {
@@ -59,15 +118,15 @@ api.use(function (req, res, next) {
                 next(err);
             } else {
                 if (user != null) {
-                    req.User = user;
+                    req.user = user;
                     next();
                 } else {
-                    next(new Error("No existe el usuario"));
+                    next(new Error(i18n.__('userNotFound')));
                 }
             }
         });
     } else {
-        next(new Error("No apikey"));
+        next(new Error(i18n.__('noApiKey')));
     }
 });
 
@@ -76,14 +135,13 @@ api.use(function (req, res, next) {
 //api.use('/collections', apiUserRouter);
 //api.use('/user', apiUserRouter);
 
-// Urls
-api.get('/', function (req, res, next) {
-    res.json(null);
-});
-
+// Error 403
 api.use(function (error, request, response, next) {
-    response.status(403);
-    response.render("error", {errorCode: 403});
+    response.json({
+        'status': 'ko',
+        'code': 403,
+        'message': error
+    });
 });
 
 module.exports = api;
