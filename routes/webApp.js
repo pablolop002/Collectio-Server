616f89ab6c230b0bbaaaa3b164f28b24db1ea52a
pablolop002 <pablolop002@me.com>
2021-08-07 14:24:20 +0200

    Web - Login page and Microsoft sign in

--- routes/webApp.js
+++ routes/webApp.js
@@ -13,6 +13,10 @@ const users = require("./webApp/user");
 // Router
 const webApp = express.Router();
 
+webApp.get("/login", function (request, response) {
+  response.render("pages/sign-in", { current: "sign-in" });
+});
+
 // Google Auth
 webApp.get(
   "/login/google",
@@ -33,13 +37,24 @@ webApp.get(
 );
 
 // Apple Auth
-webApp.get("/login/Apple", passport.authenticate("Apple"));
+webApp.get("/login/apple", passport.authenticate("Apple"));
 
-webApp.post("/login/Apple/callback", function (request, response, next) {
+webApp.post("/login/apple/callback", function (request, response, next) {
   passport.authenticate("Apple", function (err, user, info) {
     response.redirect("/");
   })(request, response, next);
 });
+
+// Microsoft Auth
+webApp.get("/login/microsoft", passport.authenticate("Microsoft"));
+
+webApp.get(
+  "/login/microsoft/callback",
+  passport.authenticate("Microsoft", { failureRedirect: "/login" }),
+  function (request, response) {
+    response.redirect("/");
+  }
+);
 
 // Sign Out
 webApp.get("/logout", function (request, response, next) {
